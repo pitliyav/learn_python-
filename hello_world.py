98bc2428df6614f70d1d52f0336f23124c300281
pitliyav <vivek.pitliya@gmail.com>
2021-11-05 16:47:41 +0530

    Part of Conquering The World

--- hello_world.py
+++ hello_world.py
@@ -2,4 +2,5 @@ print("Hello World !")
 """   
 Changes as a part of AD 710
 """
-print('Python will be conquered in a month')
+print('Python will be conquered in Nov month')
+print('Python will be conquered in Dec month')
